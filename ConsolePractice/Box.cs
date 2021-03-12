2b8d06d4278946ab750b2336da3f948828c15a2a
peterkline1150 <peterkline1150@gmail.com>
2021-03-12 17:07:18 -0600

    Add project files.

---
+++ ConsolePractice/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice
+{
+    public class Box
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Box()
+        {
+
+        }
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double CalculateVolume()
+        {
+            double volume = Length * Width * Height;
+            return volume;
+        }
+    }
+}
---
+++ ConsolePractice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            Box box2 = new Box();
+
+            Console.WriteLine("Enter the Length of box 1: ");
+            box.Length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Width of box 1: ");
+            box.Width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Height of box 1: ");
+            box.Height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Box 1 dimensions are: {box.Length} {box.Width} {box.Height}.");
+            Console.WriteLine($"Volume of box 1: {box.CalculateVolume()}\n");
+
+            Console.WriteLine("Enter the Length of box 2: ");
+            box2.Length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Width of box 2: ");
+            box2.Width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Height of box 2: ");
+            box2.Height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Box 2 dimensions are: {box2.Length} {box2.Width} {box2.Height}.");
+            Console.WriteLine($"Volume of box 2: {box2.CalculateVolume()}");
+
+            Person person = new Person();
+            person.GetSalary();
+        }
+    }
+}
